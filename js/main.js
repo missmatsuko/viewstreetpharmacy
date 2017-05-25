6939040bc667e579fdd92e7a6b8b05947f8a1ceb
Matsuko Friedland <info@matsuko.ca>
2017-05-24 21:44:39 -0700

    Adjust scroll offset for nav bar height

--- js/main.js
+++ js/main.js
@@ -4,24 +4,28 @@
 {% include js/Core/Kube.Plugin.js %}
 {% include js/Sticky/Kube.Sticky.js %}
 
-/*Add bottom border under nav when scrolled down*/
-$('#siteNav').on('fixed.sticky', function()
-{
-    $('#siteNav').addClass("scrolled");
-});
+$(function(){
 
-$('#siteNav').on('unfixed.sticky', function()
-{
-    $('#siteNav').removeClass("scrolled");
-});
+  /*Add bottom border under nav when scrolled down*/
+  $('#siteNav').on('fixed.sticky', function()
+  {
+      $('#siteNav').addClass("scrolled");
+  });
 
-/*Smooth scroll all anchor links*/
-$("a[href*='#']").click(function(e) {
-    e.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, "slow", function(){
-      window.location.hash = hash;
-    });
-});
+  $('#siteNav').on('unfixed.sticky', function()
+  {
+      $('#siteNav').removeClass("scrolled");
+  });
+
+  /*Smooth scroll all anchor links*/
+  $("a[href*='#']").click(function(e) {
+      e.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top-$(siteNav).innerHeight()
+      }, "slow", function(){
+        window.location.hash = hash;
+      });
+  });
+
+})
