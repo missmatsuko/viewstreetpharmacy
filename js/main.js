7959eadb856b35773a841aab16d7b15994a19033
Matsuko Friedland <info@matsuko.ca>
2017-05-24 21:38:31 -0700

    Add smooth scroll

--- js/main.js
+++ js/main.js
@@ -14,3 +14,14 @@ $('#siteNav').on('unfixed.sticky', function()
 {
     $('#siteNav').removeClass("scrolled");
 });
+
+/*Smooth scroll all anchor links*/
+$("a[href*='#']").click(function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, "slow", function(){
+      window.location.hash = hash;
+    });
+});
